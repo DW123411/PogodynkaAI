9b67791dd13517cbe69661807d12da4dddc715ef
DW123411 <dominikwozniak4@wp.pl>
2019-03-07 22:21:22 +0100

    Implementacja drzewa

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,26 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello3");
         System.out.println("Hello Test GitHub!");
+
+        Wezel<String> korzen = new Wezel<String>(null, "G");
+
+        Wezel<String> n1 = korzen.dodajDziecko("E");
+        Wezel<String> n2 = korzen.dodajDziecko("C");
+        Wezel<String> n3 = korzen.dodajDziecko("V");
+        n1.dodajDziecko("Z");
+        Wezel<String> n4 = n1.dodajDziecko("M");
+        n1.dodajDziecko("P");
+        n4.dodajDziecko("J");
+        Wezel<String> n5 = n2.dodajDziecko("X");
+        n5.dodajDziecko("H");
+        n5.dodajDziecko("O");
+        n3.dodajDziecko("B");
+        Wezel<String> n6 = n3.dodajDziecko("S");
+        n6.dodajDziecko("F");
+
+        Drzewo<String> drzewo = new Drzewo<String>(korzen);
+
+        System.out.print("Pre Order: ");
+        drzewo.preOrder(korzen);
     }
 }
