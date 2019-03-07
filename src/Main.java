4ea8df46712ddc08351e578293e686e80122ac64
wykrzyknik <damians13041996@gmail.com>
2019-03-07 20:45:54 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello");
         System.out.println("Hello Test GitHub!");
     }
 }
