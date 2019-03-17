3184e05620c4cda81b78e1c0288652d69c228f41
DW123411 <dominikwozniak4@wp.pl>
2019-03-17 17:41:38 +0100

    Wczytywanie drzewa z pliku - Director's Cut

--- src/Wczytywanie.java
+++ src/Wczytywanie.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Wczytywanie {
 
-    public List czytajKategoria(String sciezkadoPliku) throws FileNotFoundException {
+    /*public List czytajKategoria(String sciezkadoPliku) throws FileNotFoundException {
         File plik = new File(sciezkadoPliku);
         //Scanner skaner = new Scanner(plik);
         //String sciezka = skaner.nextLine();
@@ -45,63 +45,58 @@ public class Wczytywanie {
         }
 
         return katList;
-    }
+    }*/
 
     public static Drzewo<ElementDrzewa> wczytajDrzewoZPliku(String sciezka){
         File plik = new File(sciezka);
         Scanner skaner = null;
         try {
-            skaner = new Scanner(plik);
+            skaner = new Scanner(plik); //inicjalizacja Scannera
         }catch(FileNotFoundException err){
             System.out.println(err.toString());
         }
-        List<Wezel<ElementDrzewa>> wezly = new ArrayList<>();
+        List<Wezel<ElementDrzewa>> wezly = new ArrayList<>(); //lista zawierająca węzły wczytywanego drzewa
         Drzewo<ElementDrzewa> drzewo = new Drzewo<>();
-        try {
-            skaner = new Scanner(plik);
-        }catch(FileNotFoundException err){
-            System.out.println(err.toString());
-        }
         try{
-            boolean czyPierwszy = true;
+            boolean czyPierwszy = true; //zmienna potrzebna do wykrycia pierwszej linii danych w pliku
             while (skaner.hasNextLine()){
-                int tmp = skaner.nextInt();
-                if (tmp == 0) {
-                    Kategoria tmpKat = new Kategoria(skaner.next());
-                    String tmpNazwaRodzica = skaner.next();
+                int tmp = skaner.nextInt(); //wczytanie liczby określającej typ danych (Kategoria lub Parametr)
+                if (tmp == 0) { //Kategoria
+                    Kategoria tmpKat = new Kategoria(skaner.next()); //utworzenie kategorii o nazwie wczytanej z pliku
+                    String tmpNazwaRodzica = skaner.next(); //wczytanie nazwy rodzica Kategorii w drzewie
                     Wezel<ElementDrzewa> tmpRodzic = null;
                     for(Wezel<ElementDrzewa> obj : wezly){
-                        if((obj.getDane().getNazwa().equals(tmpNazwaRodzica))){
+                        if((obj.getDane().getNazwa().equals(tmpNazwaRodzica))){ //przeszukanie wczytanych już wcześniej węzłów w celu znalezienia rodzica dla nowo wczytanej kategorii
                             tmpRodzic = obj;
                         }
                     }
-                    Wezel<ElementDrzewa> tmpWezel = new Wezel<>(tmpRodzic,tmpKat);
+                    Wezel<ElementDrzewa> tmpWezel = new Wezel<>(tmpRodzic,tmpKat); //utworzenie węzła drzewa
                     wezly.add(tmpWezel);
-                    if (czyPierwszy){
+                    if (czyPierwszy){ //w przypadku pierwszej Kategorii należy utworzyć nowy objekt klasy Drzewo
                         drzewo = new Drzewo<>(tmpWezel);
                         czyPierwszy = false;
                     }
                     if(tmpRodzic!=null){
-                        tmpRodzic.dodajDziecko(tmpWezel);
+                        tmpRodzic.dodajDziecko(tmpWezel); //dodanie utworzonego węzła do listy potomków jego rodzica w drzewie
                     }
-                } else {
+                } else { //Parametr
                     Parametr tmpPar = null;
-                    if(skaner.nextInt()==0) {
+                    if(skaner.nextInt()==0) { //jeśli drugą wczytaną wartością jest 0 to oznacza że parametr ma wartość boolean
                         tmpPar = new Parametr(skaner.next(),skaner.nextBoolean());
-                    }else{
+                    }else{ //jeśli druga wczytana wartość wynosi 1 to oznacza że parametr ma wartość liczbową
                         tmpPar = new Parametr(skaner.next(),skaner.nextInt());
                     }
-                    String tmpNazwaRodzica = skaner.next();
+                    String tmpNazwaRodzica = skaner.next(); //wczytanie nazwy rodzica Parametru w drzewie
                     Wezel<ElementDrzewa> tmpRodzic = null;
                     for(Wezel<ElementDrzewa> obj : wezly){
-                        if((obj.getDane().getNazwa().equals(tmpNazwaRodzica))){
+                        if((obj.getDane().getNazwa().equals(tmpNazwaRodzica))){ //przeszukanie wczytanych już wcześniej węzłów w celu znalezienia rodzica dla nowo wczytanego parametru
                             tmpRodzic = obj;
                         }
                     }
-                    Wezel<ElementDrzewa> tmpWezel = new Wezel<>(tmpRodzic,tmpPar);
+                    Wezel<ElementDrzewa> tmpWezel = new Wezel<>(tmpRodzic,tmpPar); //utworzenie węzła drzewa
                     wezly.add(tmpWezel);
                     if(tmpRodzic!=null){
-                        tmpRodzic.dodajDziecko(tmpWezel);
+                        tmpRodzic.dodajDziecko(tmpWezel); //dodanie utworzonego węzła do listy potomków jego rodzica w drzewie
                     }
                 }
             }
