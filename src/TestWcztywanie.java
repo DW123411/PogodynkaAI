902a65417a7d04910850d4b0c544dd9a07f18542
TheKubar <44845050+TheKubar@users.noreply.github.com>
2019-03-25 18:18:24 +0100

    Add files via upload
    Test Wczytywania V.1

---
+++ src/TestWcztywanie.java
@@ -0,0 +1,53 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class TestWcztywanie {
+        Wczytywanie unit = new Wczytywanie();
+    @Test
+    public void loadFile()
+    {        
+        Wczytywanie unit = new Wczytywanie();
+        unit.wczytajDrzewoZPliku("src/test.txt");
+        assertEquals("0", unit.wezly.get(0));
+        assertEquals("Outlook", unit.wezly.get(1));
+        assertEquals("null", unit.wezly.get(2));
+        assertEquals("1", unit.wezly.get(3));
+        assertEquals("0", unit.wezly.get(4));
+        assertEquals("Sunny", unit.wezly.get(5));
+        assertEquals("true", unit.wezly.get(6));
+        assertEquals("Outlook", unit.wezly.get(7));
+        assertEquals("1", unit.wezly.get(8));
+        assertEquals("0", unit.wezly.get(9));
+        assertEquals("Cloudy", unit.wezly.get(10));
+        assertEquals("false", unit.wezly.get(11));
+        assertEquals("Outlook", unit.wezly.get(12));
+    }
+    @Test(expected=IOException.class)
+    public void czyPlikJestPusty() throws IOException {
+
+        // Jesli tresc pliku nie istnieje (jest pusta)
+        System.out.println("readFile");
+        unit.wczytajDrzewoZPliku("src/test.txt");
+        Drzewo<ElementDrzewa> result = unit.wczytajDrzewoZPliku("src/test.txt");
+    }
+    //Zastosowanie asercji @Test(excepcted=tunazwaklasywyjatku.class) buduje test (metodę), której wywołanie niezakończone wyrzuceniem wyjątku będzie uznawane za porażkę testu. 
+    //Jest to bardzo użyteczny sposób na ich testowanie.
+
+    @Test
+    public void SprawdzanieTreści () throws Exception {
+
+        // Jesli tresc pliku to 'Outlook'
+           unit.wczytajDrzewoZPliku("test.txt");
+           Drzewo<ElementDrzewa> result = unit.wczytajDrzewoZPliku("src/test.txt");
+           assertEquals("test", result);
+
+        }
+    }     
+
