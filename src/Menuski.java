4a1fed5f052ba5fa1fc3ac07692ab5d25ff6be4f
madman <awitasiak@gmail.com>
2019-03-25 18:11:09 +0100

    Dodanie nowych opcji menu

--- src/Menuski.java
+++ src/Menuski.java
@@ -9,18 +9,25 @@ import javax.swing.JTextField;
 public class Menuski extends JMenuBar
 {
     JMenu operacje = new JMenu ("Operacje...");
-    
-    
+
     JMenuItem tree = new JMenuItem("Wczytaj drzewo z Pliku");
     
     JMenuItem showtree = new JMenuItem("Rysuj Drzewo");
 
+    JMenuItem exit = new JMenuItem("Wyjdź z programu");
 
+    JMenuItem credits = new JMenuItem("O programie");
+
+    JMenuItem clean = new JMenuItem("Wyczyść");
     public Menuski()
     {         
         //JPanel rozklad = new JPanel();
         operacje.add(showtree);
         operacje.add(tree);
+        operacje.add(clean);
+        operacje.add(credits);
+
+        operacje.add(exit);
        
         add(operacje);
         
