88dc9bfd799ad6ba6d39551768cdca3d0a108ae2
DW123411 <dominikwozniak4@wp.pl>
2019-03-25 11:49:49 +0100

    Wartosc Atrybutu - ilość true i false

--- src/WartoscAtrybutu.java
+++ src/WartoscAtrybutu.java
@@ -3,24 +3,42 @@ public class WartoscAtrybutu implements ElementDrzewa {
     private boolean wartosc;
     private boolean czyLiczba;
     private int wartoscLiczbowa;
+    private int iloscTrue;
+    private int iloscFalse;
 
     public WartoscAtrybutu(){
         nazwa = "";
         wartosc = false;
         czyLiczba = false;
         wartoscLiczbowa = 0;
+        iloscTrue = 0;
+        iloscFalse = 0;
     }
 
     public WartoscAtrybutu(String nazwa, boolean wartosc){
         this.nazwa = nazwa;
         this.wartosc = wartosc;
         czyLiczba = false;
+        if(wartosc){
+            iloscTrue = 1;
+            iloscFalse = 0;
+        }else{
+            iloscTrue = 0;
+            iloscFalse = 1;
+        }
     }
 
     public WartoscAtrybutu(String nazwa, int wartoscLiczbowa){
         this.nazwa = nazwa;
         this.wartoscLiczbowa = wartoscLiczbowa;
         czyLiczba = true;
+        if(wartosc){
+            iloscTrue = 1;
+            iloscFalse = 0;
+        }else{
+            iloscTrue = 0;
+            iloscFalse = 1;
+        }
     }
 
     public void setNazwa(String nazwa){
@@ -39,6 +57,14 @@ public class WartoscAtrybutu implements ElementDrzewa {
         this.wartoscLiczbowa = wartoscLiczbowa;
     }
 
+    public void setIloscTrue(int iloscTrue){
+        this.iloscTrue = iloscTrue;
+    }
+
+    public void setIloscFalse(int iloscFalse){
+        this.iloscFalse = iloscFalse;
+    }
+
     public String getNazwa(){
         return nazwa;
     }
@@ -53,6 +79,22 @@ public class WartoscAtrybutu implements ElementDrzewa {
 
     public int getWartoscLiczbowa(){
         return wartoscLiczbowa;
+    }
+
+    public int getIloscTrue(){
+        return iloscTrue;
+    }
+
+    public int getIloscFalse(){
+        return iloscFalse;
+    }
+
+    public void iloscTrueInc(){
+        iloscTrue++;
+    }
+
+    public void iloscFalseInc(){
+        iloscFalse++;
     }
 
     public String toString(){
