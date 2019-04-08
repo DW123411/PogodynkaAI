52f6d8987c6e6c4051da0ff97732497e5c756c47
wykrzyknik <damians13041996@gmail.com>
2019-04-08 17:08:45 +0200

    Zabezpieczenie wczytywania przed pustym lub nieodpowiednimi danymi

--- src/Wczytywanie.java
+++ src/Wczytywanie.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Wczytywanie {
@@ -13,65 +11,85 @@ public class Wczytywanie {
     public static Drzewo<ElementDrzewa> wczytajDrzewoZPliku(String sciezka){
         File plik = new File(sciezka);
         Scanner skaner = null;
+
         try {
             skaner = new Scanner(plik); //inicjalizacja Scannera
         }catch(FileNotFoundException err){
             System.out.println(err.toString());
         }
+        if(plik.length()==0) {
+            JOptionPane.showMessageDialog(null, "Wrzucasz pusty plik");
+
+        }
+
         List<Wezel<ElementDrzewa>> wezly = new ArrayList<>(); //lista zawierająca węzły wczytywanego drzewa
         Drzewo<ElementDrzewa> drzewo = new Drzewo<>();
-        try{
+        try {
             skaner.useDelimiter("\\s*,\\s*");
             boolean czyPierwszy = true; //zmienna potrzebna do wykrycia pierwszej linii danych w pliku
-            while (skaner.hasNextLine()){
+            boolean blad = false; // zmienna do błędu
+            while (skaner.hasNextLine()) {
+
                 int tmp = skaner.nextInt(); //wczytanie liczby określającej typ danych (Atrybut lub WartoscAtrybutu)
                 if (tmp == 0) { //Atrybut
                     Atrybut tmpAtr = new Atrybut(skaner.next()); //utworzenie kategorii o nazwie wczytanej z pliku
                     String tmpNazwaRodzica = skaner.next(); //wczytanie nazwy rodzica Atrybutu w drzewie
                     Wezel<ElementDrzewa> tmpRodzic = null;
-                    for(Wezel<ElementDrzewa> obj : wezly){
-                        if((obj.getDane().getNazwa().equals(tmpNazwaRodzica))){ //przeszukanie wczytanych już wcześniej węzłów w celu znalezienia rodzica dla nowo wczytanego atrybutu
+                    for (Wezel<ElementDrzewa> obj : wezly) {
+                        if ((obj.getDane().getNazwa().equals(tmpNazwaRodzica))) { //przeszukanie wczytanych już wcześniej węzłów w celu znalezienia rodzica dla nowo wczytanego atrybutu
                             tmpRodzic = obj;
                         }
                     }
-                    Wezel<ElementDrzewa> tmpWezel = new Wezel<>(tmpRodzic,tmpAtr); //utworzenie węzła drzewa
+                    Wezel<ElementDrzewa> tmpWezel = new Wezel<>(tmpRodzic, tmpAtr); //utworzenie węzła drzewa
                     wezly.add(tmpWezel);
-                    if (czyPierwszy){ //w przypadku pierwszego Atrybutu należy utworzyć nowy objekt klasy Drzewo
+                    if (czyPierwszy) { //w przypadku pierwszego Atrybutu należy utworzyć nowy objekt klasy Drzewo
                         drzewo = new Drzewo<>(tmpWezel);
                         czyPierwszy = false;
                     }
-                    if(tmpRodzic!=null){
+                    if (tmpRodzic != null) {
                         tmpRodzic.dodajDziecko(tmpWezel); //dodanie utworzonego węzła do listy potomków jego rodzica w drzewie
                     }
+
+
                 } else { //WartoscAtrybutu
                     WartoscAtrybutu tmpWA = null;
-                    if(skaner.nextInt()==0) { //jeśli drugą wczytaną wartością jest 0 to oznacza że WartoscAtrybutu ma wartość boolean
-                        tmpWA = new WartoscAtrybutu(skaner.next(),skaner.nextBoolean());
-                    }else{ //jeśli druga wczytana wartość wynosi 1 to oznacza że WartoscAtrybutu ma wartość liczbową
-                        tmpWA = new WartoscAtrybutu(skaner.next(),skaner.nextInt());
+                    if (skaner.nextInt() == 0) { //jeśli drugą wczytaną wartością jest 0 to oznacza że WartoscAtrybutu ma wartość boolean
+                        tmpWA = new WartoscAtrybutu(skaner.next(), skaner.nextBoolean());
+                    } else { //jeśli druga wczytana wartość wynosi 1 to oznacza że WartoscAtrybutu ma wartość liczbową
+                        tmpWA = new WartoscAtrybutu(skaner.next(), skaner.nextInt());
                     }
                     String tmpNazwaRodzica = skaner.next(); //wczytanie nazwy rodzica WartosciAtrybutu w drzewie
                     Wezel<ElementDrzewa> tmpRodzic = null;
-                    for(Wezel<ElementDrzewa> obj : wezly){
-                        if((obj.getDane().getNazwa().equals(tmpNazwaRodzica))){ //przeszukanie wczytanych już wcześniej węzłów w celu znalezienia rodzica dla nowo wczytanej WartosciAtrybutu
+                    for (Wezel<ElementDrzewa> obj : wezly) {
+                        if ((obj.getDane().getNazwa().equals(tmpNazwaRodzica))) { //przeszukanie wczytanych już wcześniej węzłów w celu znalezienia rodzica dla nowo wczytanej WartosciAtrybutu
                             tmpRodzic = obj;
                         }
                     }
-                    Wezel<ElementDrzewa> tmpWezel = new Wezel<>(tmpRodzic,tmpWA); //utworzenie węzła drzewa
+                    Wezel<ElementDrzewa> tmpWezel = new Wezel<>(tmpRodzic, tmpWA); //utworzenie węzła drzewa
                     wezly.add(tmpWezel);
-                    if(tmpRodzic!=null){
+                    if (tmpRodzic != null) {
                         tmpRodzic.dodajDziecko(tmpWezel); //dodanie utworzonego węzła do listy potomków jego rodzica w drzewie
+                    } else if (tmpRodzic == null) {
+                        JOptionPane.showMessageDialog(null, "Nieprawidlowe dane , Wezel musi mieć rodziców");
+                        blad = true;
+                        break;
                     }
                 }
+
+            if (blad != true) {
+                JOptionPane.showMessageDialog(null, wezly.toString());
             }
+        }
             skaner.close();
         }catch(NullPointerException err){
             System.out.println(err.toString());
         }
         catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Nieprawidlowe dane");
+           JOptionPane.showMessageDialog(null, "Nieprawidlowe dane");
             System.out.println(e.toString());
         }
+
+
         return drzewo;
     }
     /**
@@ -148,5 +166,5 @@ public class Wczytywanie {
         KS.set_klasyfikacja_tablica(temp_table);
     return KS;
     }
-    
+
 }
