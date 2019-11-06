e6cd321de06be1173e92c1b910fa9d3646e34749
Madman <awitasiak@gmail.com>
2019-11-06 14:35:59 +0100

    Poprawka z sypanda nullami do tabeli Teraz sypie się w innym miejscu

--- src/PodzialUczTest.java
+++ src/PodzialUczTest.java
@@ -1,4 +1,7 @@
+import java.awt.desktop.SystemEventListener;
+
 public class PodzialUczTest {
+
 
     public PodzialUczTest(){
     }
@@ -6,14 +9,14 @@ public class PodzialUczTest {
     public static DaneWejsciowe losowanietest(int ilosc, DaneWejsciowe org){
         int g = 0;
         int t = 0;
+        DaneWejsciowe test = new DaneWejsciowe();
 
         int[] tr1 =null;
         int[] tr2 =null;
 
-        DaneWejsciowe test = new DaneWejsciowe();
 
         String[][] element = org.get_klasyfikacja_string();
-        String[][] wylosowany = new String[ilosc][element[0].length];
+        String[][] wylosowany = new String[ilosc][element[0].length-1];
 
         for(g=0; g<ilosc;g++){
                for(t=0; t<element[0].length-1; t++){
@@ -21,6 +24,7 @@ public class PodzialUczTest {
                     wylosowany[g][t]=element[r1][t];
                 }
             }
+            ;
 
 
 
@@ -32,25 +36,52 @@ public class PodzialUczTest {
     public static DaneWejsciowe losowanieucz(int ilosc, DaneWejsciowe org){
         int g = 0;
         int t = 0;
+        int c = 0;
+        int a = 0;
+        int ilosctest=ilosc;
+
 
         int[] tr1 =null;
         DaneWejsciowe ucz = new DaneWejsciowe();
 
         String[][] element = org.get_klasyfikacja_string();
-        String[][] wylosowany = new String[ilosc][element[0].length];
+        ElementDrzewa[][] wylosowany = new ElementDrzewa[ilosc+1][element[1].length];
 
-        for(g=0; g<ilosc;g++){
-            for(t=0; t<element[0].length; t++){
-                int r1 = (int) (Math.random() * (element.length));
-                wylosowany[g][t]=element[r1][t];
-            }
+        while(c<element[0].length){
+            Atrybut ser = new Atrybut (element[0][c]);
+            wylosowany[0][c] = ser;
+         //   System.out.print(wylosowany[0][c]);
+            c++;
         }
 
-            ucz.set_klasyfikacja_tablica_string(wylosowany);
+
+        for(g=0; g<ilosctest;g++){
+            int r1 = (int) (Math.random() * (element.length));
+           // System.out.println(" ");
+            for(t=0;t<element[0].length;t++){
+                 if(r1!=0){
+                     if(t<element[0].length-1){
+                     wylosowany[g+1][t]=new WartoscAtrybutu(element[r1][t]);
+                     }
+                     else if(t==element[0].length-1){
+                         wylosowany[g+1][t]=new Decyzja(element[r1][t]);
+                     }
+                   // System.out.print(wylosowany[g+1][t]+",");
+                 }
+                 else r1=(int) (Math.random()*(element.length));
+
+                 }
+    }
+            for(int s=0;s<wylosowany.length;s++){
+                System.out.println(" ");
+                for(int o=0;o<wylosowany[0].length; o++){
+                    System.out.print(wylosowany[s][o]+",");
+                }
+            }
+            ucz.set_klasyfikacja_tablica(wylosowany);
 
         return ucz;
     }
-
 
 
 
