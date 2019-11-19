d3086d43e16ceb411c941d445611d76d42e94db2
sk9hobbit <48244897+sk9hobbit@users.noreply.github.com>
2019-11-19 01:26:06 +0100

    dokladnosc w Okno. + (accuracy+)

--- src/Okno.java
+++ src/Okno.java
@@ -125,6 +125,7 @@ public class Okno extends JFrame implements ActionListener
         menu.jpeg.addActionListener(this);
                 menu.show_klasyfikacja.addActionListener(this);
         menu.skalowanie.addActionListener(this);
+		 menu.accuracy.addActionListener(this);
         wyczysc.addActionListener(this);
         zapisz.addActionListener(this);
         zapisz2.addActionListener(this);
@@ -173,17 +174,22 @@ public class Okno extends JFrame implements ActionListener
                 wyswietlanie.rysujPrzyciski(wyswietlanie.listaButton);
             }
         }
+		   else if (zrodlo==menu.accuracy){
+            
+            new dokladnosc();
+            
+        }
          else if(zrodlo==menu.decyzja_okno){
                 if(daneWejsciowe!=null){
            
                     try{
                     TableRenderDemo.createAndShowGUI(daneWejsciowe);}
                     catch(Exception de){
-           JOptionPane.showMessageDialog(null, "Nie wczytałeś klasyfikacji.");                        
+           JOptionPane.showMessageDialog(null, "Error");                        
                     }
                 }
             else{
-           JOptionPane.showMessageDialog(null, "Error.");
+           JOptionPane.showMessageDialog(null, "Nie wczytałeś klasyfikacji.");
             }
         }
 
@@ -511,5 +517,78 @@ public class Okno extends JFrame implements ActionListener
 
     public void setDaneWejsciowe(DaneWejsciowe daneWejsciowe){
         this.daneWejsciowe = daneWejsciowe;
+    }
+	
+	
+    private class dokladnosc extends JFrame  implements ActionListener{
+
+        private JLabel label1;    private JLabel label2;    private JButton buton;
+        @Override
+        public void actionPerformed(ActionEvent e){
+            Object zrodlo = e.getSource();
+
+            if(zrodlo == buton){
+                dispose();
+                
+            }
+        }
+
+        private void dopasujSieDoZawartosci()
+        {
+            //dostosowanie okna do zawartości
+            pack();   
+            //wyśrodkowanie ramki
+            setLocationRelativeTo(null);           
+        }
+
+        public dokladnosc(){
+            buton = new JButton("OK");
+            label1 = new JLabel("Dokładność ucząca : ");
+            label2 = new JLabel("Dokładność testująca : ");
+            setSize( 250, 250);  // ustawienie rozmiarow okna
+            setLocation(500,500);
+            // Pozyskanie powierzchni zawartości
+            Container  contents = getContentPane();     
+            // Utworzenie własnego panela z powierzchnią do rysowania figur
+            // i dodanie go do powierzchni (zawartości) okna
+            //setLayout(new GridLayout( 10, 50));
+            setLayout( null );
+            label1.setSize(200,25);  label1.setLocation(10,50);
+
+            label2.setSize(200,25);  label2.setLocation(10,75);
+
+            buton.setSize(100,25);  buton.setLocation(50,100);
+            add(label1);
+             add(label2);
+            buton.addActionListener(this);
+
+            add(buton);
+
+            setVisible(true);
+           
+        }
+
+        /**
+         * metoda ustawia napis labela z atrybutem wejsciowym 
+         * 1 dla pierwszego(uczaca dokladnosc) 
+         * 2 dla drugiego (tesutjaca dokladnosc) 
+         */
+        public void setlabel(int l, String a){
+
+            if(l==1){
+                //dokladnosc uczaca
+                this.label1= new JLabel(a);
+            }
+            if(l==2){
+                //dokladnosc testujaca 
+                this.label2= new JLabel(a);
+            }
+
+        }
+        
+        public void popup(){
+            
+        }
+
     }
 }
